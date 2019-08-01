7d71ebd32881b9538436b86e14b3896721c5f7b7
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2019-07-31 20:53:16 -0400

    Added Camera Transform

--- src/pointcloud_analysis.cpp
+++ src/pointcloud_analysis.cpp
@@ -1,5 +1,6 @@
 #include "tic_toc.h"
 
+#include <math.h>
 #include <ros/ros.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
@@ -7,14 +8,16 @@
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/common/transforms.h>
 #include <pcl/features/normal_3d.h>
-#include <pcl/features/integral_image_normal.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
 using namespace pcl;
 
-ros::Publisher pub;
+static const double CAMERA_ANGLE = M_PI / 4;
+static const double CAMERA_HEIGHT = 0.10;
 
+ros::Publisher pub_cloud;
 visualization::PCLVisualizer::Ptr viewer;
 
 visualization::PCLVisualizer::Ptr createViewer() {
@@ -59,11 +62,34 @@ void statOutRemoval(PointCloud<PointXYZRGB>::Ptr cloud) {
   sor.filter(*cloud);
 }
 
-void downSample(PointCloud<PointXYZRGB>::Ptr cloud, double xDim, double yDim, double zDim) {
+void downSample(PointCloud<PointXYZRGB>::Ptr cloud, double x_dim, double y_dim, double z_dim) {
   VoxelGrid<PointXYZRGB> vox;
   vox.setInputCloud(cloud);
-  vox.setLeafSize(xDim, yDim, zDim);
+  vox.setLeafSize(x_dim, y_dim, z_dim);
   vox.filter(*cloud);
+}
+
+PointCloud<PointXYZRGB>::Ptr frameTransform(PointCloud<PointXYZRGB>::Ptr cloud) {
+  Eigen::Affine3f camera_transform_matrix = Eigen::Affine3f::Identity();
+  camera_transform_matrix.translation() << 0.0, -CAMERA_HEIGHT, 0.0;
+  camera_transform_matrix.rotate (Eigen::AngleAxisf (-CAMERA_ANGLE, Eigen::Vector3f::UnitX()));
+  
+  PointCloud<PointXYZRGB>::Ptr cloud_angle_corrected (new PointCloud<PointXYZRGB> ());
+  transformPointCloud(*cloud, *cloud_angle_corrected, camera_transform_matrix);
+
+  // Normalized Gravity Vector (Get from IMU)
+  double gravity_x = 0.0;
+  double gravity_y = -1.0;
+  double gravity_z = 0.0;
+
+  double cross_x = -gravity_z;
+  double cross_y = 0;
+  double cross_z = -gravity_x;
+
+  Eigen::Matrix4f gravity_transform_matrix = Eigen::Matrix4f::Identity();
+  gravity_transform_matrix (0, 0) = 
+
+  return transformed_cloud;
 }
 
 PointCloud<Normal>::Ptr computeNormals(PointCloud<PointXYZRGB>::Ptr cloud, double radius) {
@@ -92,15 +118,15 @@ PointCloud<Normal>::Ptr computeNormals(PointCloud<PointXYZRGB>::Ptr cloud, doubl
   return normals;
 }*/
 
-PointCloud<PointXYZI> normal_analysis(PointCloud<Normal> normals, PointCloud<PointXYZRGB> cloud) {
+PointCloud<PointXYZI> normalAnalysis(PointCloud<Normal> normals, PointCloud<PointXYZRGB> cloud) {
   PointCloud<PointXYZI> output_cloud;
   output_cloud.height = cloud.height;
   output_cloud.width = cloud.width;
   output_cloud.header.frame_id = cloud.header.frame_id;
 	
   float vector_x = 0.0;
-  float vector_y = 0.0;
-  float vector_z = -1.0;
+  float vector_y = 1.0;
+  float vector_z = 0.0;
 
   float threshold = 0.5;
 
@@ -133,34 +159,19 @@ PointCloud<PointXYZI> normal_analysis(PointCloud<Normal> normals, PointCloud<Poi
 }
 
 void pointcloudCallback(const PointCloud<PointXYZRGB>::ConstPtr& cloud) {
-  PointCloud<PointXYZRGB>::Ptr cloud_conv (new PointCloud<PointXYZRGB> (*cloud));
-  ROS_ERROR("SIZE1: %lu", cloud_conv->points.size());
+  PointCloud<PointXYZRGB>::Ptr cloud_converted (new PointCloud<PointXYZRGB> (*cloud));
 
-  tic(); 
-  cutOff(cloud_conv);
-  ROS_ERROR("CUTOFF: %f", toc());
+  cutOff(cloud_converted);
+  downSample(cloud_converted, 0.05, 0.05, 0.05);
+  //statOutRemoval(cloud_converted);
 
-  //tic();
-  //statOutRemoval(cloud_conv);
-  //ROS_ERROR("STAT: %f", toc());
+  PointCloud<PointXYZRGB>::Ptr cloud_transformed = frameTransform(cloud_converted);
 
-  tic();
-  downSample(cloud_conv, 0.05, 0.05, 0.05);
-  ROS_ERROR("DOWNSAMPLE: %f", toc());
+  PointCloud<Normal>::Ptr cloud_normals = computeNormals(cloud_transformed, 0.05);
+  PointCloud<PointXYZI> output = normalAnalysis(*cloud_normals, *cloud_transformed);
 
-  ROS_ERROR("SIZE2: %lu", cloud_conv->points.size());
-
-  tic();
-  PointCloud<Normal>::Ptr cloud_normals = computeNormals(cloud_conv, 0.05);
-  ROS_ERROR("NORMALS: %f", toc());
-
-  ROS_ERROR("NORMAL SIZE: %lu", cloud_normals->points.size());
-
-  PointCloud<PointXYZI> output = normal_analysis(*cloud_normals, *cloud_conv);
-
-  pub.publish(output);
-
-  //updateViewer(viewer, cloud_conv, cloud_normals);
+  pub_cloud.publish(output);
+  //updateViewer(viewer, cloud_transformed, cloud_normals);
 }
 
 int main(int argc, char **argv) {
@@ -171,7 +182,7 @@ int main(int argc, char **argv) {
   ros::Subscriber sub_cloud =
 	  n.subscribe<PointCloud<PointXYZRGB>>("/camera/depth_registered/points", 1, pointcloudCallback);
 
-  pub = n.advertise<PointCloud<PointXYZI>>("/normal_processed/points", 1);
+  pub_cloud = n.advertise<PointCloud<PointXYZI>>("/normal_processed/points", 1);
 
   ros::spin();
   return 0;
