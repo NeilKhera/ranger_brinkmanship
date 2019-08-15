7959bc3a6fcba7034792659bd6202d1ab9baf28b
Neil Khera <32021246+Nekhera@users.noreply.github.com>
2019-08-15 14:55:36 -0400

    Code Refactoring

--- src/pointcloud_analysis.cpp
+++ src/pointcloud_analysis.cpp
@@ -6,11 +6,9 @@
 #include <pcl/common/transforms.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/filters/passthrough.h>
-#include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
 #include <pcl/point_types.h>
-#include <pcl/visualization/pcl_visualizer.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/kdtree/kdtree_flann.h>
@@ -30,25 +28,7 @@ float roll = 0.0;
 float pitch = 0.0;
 
 ros::Publisher pub_cloud;
-ros::Publisher pub_cloud_obstacles;
 ros::Publisher pub_go;
-visualization::PCLVisualizer::Ptr viewer;
-
-visualization::PCLVisualizer::Ptr createViewer() {
-  visualization::PCLVisualizer::Ptr viewer(new visualization::PCLVisualizer("3D Viewer"));
-  viewer->setBackgroundColor(0, 0, 0);
-  viewer->addCoordinateSystem(1.0);
-  viewer->initCameraParameters();
-  return viewer;
-}
-
-void updateViewer(visualization::PCLVisualizer::Ptr viewer, PointCloud<PointXYZRGB>::Ptr cloud, PointCloud<Normal>::Ptr normals) {
-  viewer->removePointCloud("cloud");
-  viewer->addPointCloud<PointXYZRGB>(cloud, "cloud");
-  viewer->removePointCloud("normals");
-  viewer->addPointCloudNormals<PointXYZRGB, Normal>(cloud, normals, 1, 0.1, "normals");
-  viewer->spinOnce();
-}
 
 void cutOff(PointCloud<PointXYZRGB>::Ptr cloud) {
   PassThrough<PointXYZRGB> pt;
@@ -56,10 +36,6 @@ void cutOff(PointCloud<PointXYZRGB>::Ptr cloud) {
 
   pt.setFilterFieldName("x");
   pt.setFilterLimits(-ROVER_WIDTH / 2, ROVER_WIDTH / 2);
-  pt.filter(*cloud);
-
-  pt.setFilterFieldName("y");
-  pt.setFilterLimits(Y_MIN, Y_MAX);
   pt.filter(*cloud);
 }
 
@@ -74,7 +50,6 @@ PointCloud<PointXYZRGB>::Ptr frameTransform(PointCloud<PointXYZRGB>::Ptr cloud) 
   Eigen::Affine3f transform_matrix = Eigen::Affine3f::Identity();
   transform_matrix.translation() << 0.0, -CAMERA_HEIGHT, 0.0;
   transform_matrix.rotate(Eigen::AngleAxisf(-CAMERA_ANGLE, Eigen::Vector3f::UnitX()));
-  transform_matrix.rotate(Eigen::AngleAxisf(roll, Eigen::Vector3f::UnitZ()));
 
   PointCloud<PointXYZRGB>::Ptr cloud_corrected (new PointCloud<PointXYZRGB>());
   transformPointCloud(*cloud, *cloud_corrected, transform_matrix);
@@ -82,18 +57,18 @@ PointCloud<PointXYZRGB>::Ptr frameTransform(PointCloud<PointXYZRGB>::Ptr cloud) 
   return cloud_corrected;
 }
 
-PointCloud<PointXYZRGB>::Ptr orientationCorrection(PointCloud<PointXYZRGB>::Ptr cloud) {
+PointCloud<PointXYZINormal>::Ptr orientationCorrection(PointCloud<PointXYZINormal>::Ptr cloud) {
   Eigen::Affine3f transform_matrix = Eigen::Affine3f::Identity();
   transform_matrix.rotate(Eigen::AngleAxisf(pitch, Eigen::Vector3f::UnitX()));
-  //transform_matrix.rotate(Eigen::AngleAxisf(roll, Eigen::Vector3f::UnitZ()));
+  transform_matrix.rotate(Eigen::AngleAxisf(roll, Eigen::Vector3f::UnitZ()));
 
-  PointCloud<PointXYZRGB>::Ptr cloud_corrected (new PointCloud<PointXYZRGB>());
-  transformPointCloud(*cloud, *cloud_corrected, transform_matrix);
+  PointCloud<PointXYZINormal>::Ptr cloud_corrected (new PointCloud<PointXYZINormal>());
+  transformPointCloudWithNormals(*cloud, *cloud_corrected, transform_matrix);
 
   return cloud_corrected;
 }
 
-float findEdge(PointCloud<PointXYZRGB> cloud) {
+float planeFalloffDistance(PointCloud<PointXYZRGB> cloud) {
   float z_min = -1.0;
 
   for (int i = 0; i < cloud.width; i += 10) {
@@ -106,31 +81,23 @@ float findEdge(PointCloud<PointXYZRGB> cloud) {
       }
     }
   }
-
   return z_min;
 }
 
-void marking(KdTreeFLANN<PointXYZI> kdtree, PointXYZI searchPoint, PointCloud<PointXYZI>::Ptr cloud, PointCloud<PointXYZI>::Ptr new_cloud, PointIndices::Ptr removals) {
-  PointXYZI new_point;
-  new_point.x = searchPoint.x;
-  new_point.y = searchPoint.y;
-  new_point.z = searchPoint.z;
-  new_point.intensity = searchPoint.intensity;
-
-  (*new_cloud).push_back(new_point);
+void marking(KdTreeFLANN<PointXYZI> kdtree, PointXYZI search_point, PointCloud<PointXYZI>::Ptr cloud, PointCloud<PointXYZI>::Ptr temp_cloud, PointIndices::Ptr removals) {
+  (*temp_cloud).push_back(search_point);
 	
   std::vector<int> pointIndices;
   std::vector<float> pointDistancesSq;
-
   float radius = 0.05;
   
-  kdtree.radiusSearch(searchPoint, radius, pointIndices, pointDistancesSq);
+  kdtree.radiusSearch(search_point, radius, pointIndices, pointDistancesSq);
   for (int i = 0; i < pointIndices.size(); i++) {
     PointXYZI point = cloud->points[pointIndices[i]];
-    if (point.intensity == 2) {
+    if (point.intensity == 1) {
       cloud->points[pointIndices[i]].intensity = 0;
       removals->indices.push_back(pointIndices[i]);
-      marking(kdtree, point, cloud, new_cloud, removals);
+      marking(kdtree, point, cloud, temp_cloud, removals);
     }
   }
 }
@@ -139,36 +106,25 @@ PointCloud<PointXYZI>::Ptr markObstacles(PointCloud<PointXYZI>::Ptr cloud) {
   KdTreeFLANN<PointXYZI> kdtree;
   kdtree.setInputCloud(cloud);
 
-  PointCloud<PointXYZI>::Ptr new_cloud(new PointCloud<PointXYZI>());
-  new_cloud->header.frame_id = cloud->header.frame_id;
-
+  PointCloud<PointXYZI>::Ptr pointholder_cloud(new PointCloud<PointXYZI>());
   PointIndices::Ptr removals(new PointIndices());
   
-  int obstacle_count = 1;
+  int obstacle_num = 1;
   for (int i = 0; i < cloud->points.size(); i++) {
     PointXYZI point = cloud->points[i];
-    if (point.intensity == 2) {
+    if (point.intensity == 1) {
       PointCloud<PointXYZI>::Ptr temp_cloud(new PointCloud<PointXYZI>());
-      temp_cloud->header.frame_id = cloud->header.frame_id;
 
-      //PointIndices::Ptr removals(new PointIndices());
-      
       cloud->points[i].intensity = 0;
       removals->indices.push_back(i);
       marking(kdtree, point, cloud, temp_cloud, removals);
 
-      //ExtractIndices<PointXYZI> extract;
-      //extract.setInputCloud(cloud);
-      //extract.setIndices(removals);
-      //extract.setNegative(true);
-      //extract.filter(*cloud);
-
       if (temp_cloud->points.size() >= 5) {
         for (int j = 0; j < temp_cloud->points.size(); j++) {
-	  temp_cloud->points[j].intensity = obstacle_count;
-          (*new_cloud).push_back(temp_cloud->points[j]);
+	  temp_cloud->points[j].intensity = obstacle_num;
+          (*pointholder_cloud).push_back(temp_cloud->points[j]);
 	}
-	obstacle_count++;
+	obstacle_num++;
       }
     }
   }
@@ -179,12 +135,11 @@ PointCloud<PointXYZI>::Ptr markObstacles(PointCloud<PointXYZI>::Ptr cloud) {
   extract.setNegative(true);
   extract.filter(*cloud);
 
-  for (int i = 0; i < new_cloud->points.size(); i++) {
-    (*cloud).push_back(new_cloud->points[i]);
+  for (int i = 0; i < pointholder_cloud->points.size(); i++) {
+    (*cloud).push_back(pointholder_cloud->points[i]);
   }
 
-
-  return new_cloud;
+  return pointholder_cloud;
 }
 
 PointCloud<Normal>::Ptr computeNormals(PointCloud<PointXYZRGB>::Ptr cloud, double radius) {
@@ -200,65 +155,56 @@ PointCloud<Normal>::Ptr computeNormals(PointCloud<PointXYZRGB>::Ptr cloud, doubl
   return normals;
 }
 
-/*PointCloud<Normal>::Ptr computeNormals(PointCloud<PointXYZRGB>::Ptr cloud,
-double depth, double smoothing) { PointCloud<Normal>::Ptr normals (new
-PointCloud<Normal>);
+PointCloud<PointXYZI>::Ptr normalAnalysis(PointCloud<Normal>::Ptr normals, PointCloud<PointXYZRGB>::Ptr cloud) {
+  PointCloud<PointXYZI>::Ptr output_cloud(new PointCloud<PointXYZI>());
+  output_cloud->header.frame_id = cloud->header.frame_id;
 
-  IntegralImageNormalEstimation<PointXYZRGB, Normal> ne;
-  ne.setNormalEstimationMethod (ne.AVERAGE_3D_GRADIENT);
-  ne.setMaxDepthChangeFactor(depth);
-  ne.setNormalSmoothingSize(smoothing);
-  ne.setInputCloud(cloud);
-  ne.compute(*normals);
-
-  return normals;
-}*/
-
-PointCloud<PointXYZI> normalAnalysis(PointCloud<Normal> normals, PointCloud<PointXYZRGB> cloud) {
-  PointCloud<PointXYZI> output_cloud;
-  output_cloud.height = cloud.height;
-  output_cloud.width = cloud.width;
-  output_cloud.header.frame_id = cloud.header.frame_id;
-
-  for (int i = 0; i < normals.points.size(); i++) {
-    float dot = -normals.points[i].normal_y;
+  for (int i = 0; i < normals->points.size(); i++) {
+    float dot = -normals->points[i].normal_y;
     float theta = std::acos(dot);
 
-    PointXYZRGB point = cloud.points[i];
+    PointXYZRGB point = cloud->points[i];
     PointXYZI p;
     p.x = point.x;
     p.y = point.y;
     p.z = point.z;
 
-    /*if (point.y > 0.15 or point.y < -0.2) {
+    if (theta > ANGLE_THRESHOLD && theta < M_PI - ANGLE_THRESHOLD) {
       p.intensity = 1;
-    } else*/ if (theta > ANGLE_THRESHOLD && theta < M_PI - ANGLE_THRESHOLD) {
-      p.intensity = 2;
     } else {
       p.intensity = 0;
     }
-
-    output_cloud.points.push_back(p);
+    output_cloud->points.push_back(p);
   }
 
   return output_cloud;
 }
 
-void goOrNoGo(PointCloud<PointXYZI> cloud, float distance) {
-  std_msgs::Bool go;
-  go.data = true;
+void surface_slope_estimation(PointCloud<PointXYZINormal>::Ptr cloud) {
+  //cutOff
 
-  /*int count = 0;
-  for (int i = 0; i < cloud.points.size(); i++) {
-    PointXYZI point = cloud.points[i];
+  float master_normal_x = 0.0f;
+  float master_normal_y = 0.0f;
+  float master_normal_z = 0.0f;
+  int count = 0;
+	
+  for (int i = 0; i < cloud->points.size(); i++) {
+    PointXYZINormal point = cloud->points[i];
+    //Ignore nan
     if (point.intensity == 0) {
+      master_normal_x += point.normal_x;
+      master_normal_y += point.normal_y;
+      master_normal_z += point.normal_z;
       count++;
     }
   }
 
-  if (count < (0.85 * cloud.points.size())) {
-    go.data = false;
-  }*/
+  ROS_ERROR("%f %f %f", master_normal_x / count, master_normal_y / count, master_normal_z / count);
+}
+
+void goOrNoGo(float distance) {
+  std_msgs::Bool go;
+  go.data = true;
 
   if (distance < EDGE_THRESHOLD) {
     go.data = false;
@@ -272,32 +218,26 @@ void orientationCallback(const geometry_msgs::Vector3Stamped::ConstPtr &vector_m
 }
 
 void pointcloudCallback(const PointCloud<PointXYZRGB>::ConstPtr &cloud) {
-  PointCloud<PointXYZRGB>::Ptr cloud_converted(
-      new PointCloud<PointXYZRGB>(*cloud));
-  
+  PointCloud<PointXYZRGB>::Ptr cloud_converted(new PointCloud<PointXYZRGB>(*cloud));
   PointCloud<PointXYZRGB>::Ptr cloud_transformed = frameTransform(cloud_converted);
 
-  float d = findEdge(*cloud_transformed);
+  float dist = planeFalloffDistance(*cloud_transformed);
 
-  downSample(cloud_transformed, 0.05, 0.05, 0.05);
-  //cutOff(cloud_transformed);
+  downSample(cloud_transformed, 0.05, 0.05, 0.05); 
+  PointCloud<Normal>::Ptr cloud_normals = computeNormals(cloud_transformed, 0.05);
+  PointCloud<PointXYZI>::Ptr cloud_analysed = normalAnalysis(cloud_normals, cloud_transformed);
+
+  PointCloud<PointXYZI>::Ptr cloud_colored(new PointCloud<PointXYZI>(*cloud_analysed));
+  PointCloud<PointXYZI>::Ptr cloud_obstacles = markObstacles(cloud_colored);
   
-  PointCloud<PointXYZRGB>::Ptr cloud_orientation_corrected = 
-	  orientationCorrection(cloud_transformed);
+  PointCloud<PointXYZINormal>::Ptr cloud_analysed_normals(new PointCloud<PointXYZINormal>());
+  concatenateFields(*cloud_normals, *cloud_analysed, *cloud_analysed_normals);
+  PointCloud<PointXYZINormal>::Ptr cloud_oriented = orientationCorrection(cloud_analysed_normals);
 
-  PointCloud<Normal>::Ptr cloud_normals =
-      computeNormals(cloud_orientation_corrected, 0.05);
-  PointCloud<PointXYZI> output =
-      normalAnalysis(*cloud_normals, *cloud_orientation_corrected);
-
-  PointCloud<PointXYZI>::Ptr output_ptr(new PointCloud<PointXYZI> (output));
-  PointCloud<PointXYZI>::Ptr obstacle_cloud = markObstacles(output_ptr);
-  pub_cloud_obstacles.publish(*obstacle_cloud);
-
-  goOrNoGo(*output_ptr, d);
-
-  pub_cloud.publish(*output_ptr);
-  //updateViewer(viewer, cloud_orientation_corrected, cloud_normals);
+  surface_slope_estimation(cloud_oriented);
+  
+  goOrNoGo(dist);
+  pub_cloud.publish(*cloud_oriented);
 }
 
 int main(int argc, char **argv) {
@@ -312,13 +252,10 @@ int main(int argc, char **argv) {
   n.getParam("/pointcloud_analysis/Y_MAX", Y_MAX);
   n.getParam("/pointcloud_analysis/Y_MIN", Y_MIN);
 
-  //viewer = createViewer();
-
   ros::Subscriber sub_orientation = n.subscribe("/ak2/imu/rpy", 1, orientationCallback);
   ros::Subscriber sub_cloud = n.subscribe<PointCloud<PointXYZRGB>>("/camera/depth_registered/points", 1, pointcloudCallback);
 
   pub_cloud = n.advertise<PointCloud<PointXYZI>>("/pointcloud/analysis/points", 1);
-  pub_cloud_obstacles = n.advertise<PointCloud<PointXYZI>>("/pointcloud/analysis/obstacles", 1);
   pub_go = n.advertise<std_msgs::Bool>("/pointcloud/analysis/go", 1);
   
   ros::spin();
